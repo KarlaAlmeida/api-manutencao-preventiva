d6610382cb63a5c14bca61a4de54d5a8b2b25fd4
KarlaAlmeida <karlakcbam@gmail.com>
2025-09-15 20:17:43 -0300

    adicionado EnderecoLoader

---
+++ karlaapitdd/src/main/java/br/edu/infnet/karlaapitdd/EnderecoLoader.java
@@ -0,0 +1,30 @@
+package br.edu.infnet.karlaapitdd;
+
+import br.edu.infnet.karlaapitdd.model.domain.entities.Endereco;
+import br.edu.infnet.karlaapitdd.model.service.EnderecoService;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnderecoLoader implements ApplicationRunner {
+
+    private final EnderecoService enderecoService;
+
+    public EnderecoLoader(EnderecoService enderecoService) {
+        this.enderecoService = enderecoService;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        Endereco endereco = enderecoService.obterEnderecoPorCep("59150015");
+
+        System.out.println("[RESULTADO] MEU CEP: " + endereco.getCep());
+        System.out.println("[RESULTADO] MEU LOGRADOURO: " + endereco.getLogradouro());
+        System.out.println("[RESULTADO] MEU COMPLEMENTO: " + endereco.getComplemento());
+        System.out.println("[RESULTADO] MEU BAIRRO: " + endereco.getBairro());
+        System.out.println("[RESULTADO] MEU MUNICÍPIO: " + endereco.getLocalidade());
+        System.out.println("[RESULTADO] MEU UF: " + endereco.getUf());
+    }
+}
